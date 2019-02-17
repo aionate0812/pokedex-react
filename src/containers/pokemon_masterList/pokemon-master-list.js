b03774bc47c129a5df7ea67073eb1fea16e1982f
nadavgree <nadavgreen@pursuit.org>
2019-02-16 19:29:49 -0500

    added load more again

--- src/containers/pokemon_masterList/pokemon-master-list.js
+++ src/containers/pokemon_masterList/pokemon-master-list.js
@@ -1,30 +1,53 @@
 import React, { Component } from 'react';
 import  Pokecard  from '../../components/pokemon-master-list'
+import axios from 'axios';
 
 class PokemonMasterList extends Component {
     constructor(props) {
         super(props)  
 
         this.state = {
-            pokemon: [1,2,3,4,5],
+            pokeList: [],
+            pokeError: {
+                status: false,
+                msg: '',
+            },
+            pokeDisplay: null
         }
     }
 
     pokeLoad = () =>{
-        this.setState({pokemon: this.state.pokemon.concat(this.state.pokemon[this.state.pokemon.length - 1]+1)})
+        let pokeOffset = 0
+        let pokeLimit = 20
+        if(this.state.pokeList.length !== 0){
+            pokeOffset = (this.state.pokeList.length + 20)
+        }
+        if(this.state.pokeList.length >= 940){
+            pokeLimit = 4
+            this.setState({pokeDisplay: "hidden"})
+        }
+        axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${pokeOffset}&limit=${pokeLimit}`)
+        .then(data=>{
+            console.log(data.data.results)
+            this.setState({pokeList: [].concat(this.state.pokeList, data.data.results)})
+        })
+        .catch(err=>{
+            console.log(err)
+            this.setState({pokeError: {status: true, msg:"Sorry Couldn't Catch 'Em All"}})
+        })
     }
 
     componentDidMount() {
-        
+        this.pokeLoad()
     }
 
     render() {
         return (
             <>
-                <div className="container" style={{"width":"50%"}}>
+                <div className="container">
                     <div className="row">
-                        <Pokecard pokemon={this.state.pokemon}/>
-                        <button className="col-6" style={{"backgroundColor":"red", "color":"white", "textAlign":"center", "cursor":"pointer"}} onClick={this.pokeLoad}>
+                        <Pokecard pokeState={this.state}/>
+                        <button className="col-12" style={{"width":"100px","backgroundColor":"red", "color":"white", "textAlign":"center", "cursor":"pointer","visibility":this.state.pokeDisplay}} onClick={this.pokeLoad}>
                             Load More...
                         </button>
                     </div>
